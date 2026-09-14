2f48f5d1a134fc7bbf94e4b5c49633250e05a0fd
ladler195-png <ladler195@gmail.com>
2026-09-14 18:58:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -606,4 +606,5 @@ if show_sidebar_content:
         st.subheader("🌀 Puzzleteil-Fragmente")
         st.caption("Sammle hier die Buchstaben (Tag 5 bis 12):")
         lab_pieces = [d for d in st.session_state.solved_doors if 5 <= d <= 12 and DOORS[d]["puzzle_piece"]]
-        if not lab_pieces:
+        if not lab_pieces: #
+            ...
